051d25e1c1ae7532d69825225f7416a549e86a3e
Artem <art.skripnikov@gmail.com>
2024-12-21 15:35:59 +0700

    h

--- build/library/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/artem/Documents/2 course/Object-Oriented Programming/ex2/library
-# Build directory: /home/artem/Documents/2 course/Object-Oriented Programming/ex2/build/library
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/tests/LifeTests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/artem/Documents/2 course/Object-Oriented Programming/ex2/build/tests/LifeTests[1]_tests.cmake")
-  include("/home/artem/Documents/2 course/Object-Oriented Programming/ex2/build/tests/LifeTests[1]_tests.cmake")
-else()
-  add_test(LifeTests_NOT_BUILT LifeTests_NOT_BUILT)
-endif()
